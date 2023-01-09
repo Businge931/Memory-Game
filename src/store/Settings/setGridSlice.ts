6086a13a3cbf3a9afbd98f91d1e4530d0d11d9a4
Businge931 <jimmybusinge9@gmail.com>
2023-01-09 02:16:45 -0800

    set global state for the grid system

---
+++ src/store/Settings/setGridSlice.ts
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { generateNumbers } from "../../utils/randomNumbers";
+
+interface gridState {
+  grid: number;
+  gridArray: number[];
+}
+const initialState: gridState = {
+  grid: 16 | 36,
+  gridArray: [],
+};
+
+export const chooseGridSlice = createSlice({
+  name: "chooseGrid",
+  initialState,
+  reducers: {
+    setGridByFour: (state) => {
+      state.grid = 16;
+      const fourGrid = generateNumbers(4, 10);
+      state.gridArray.concat(fourGrid);
+
+      // console.log(newArrFour);
+    },
+    setGridBySix: (state) => {
+      state.grid = 36;
+      const sixGrid = generateNumbers(6, 20);
+      state.gridArray.concat(sixGrid);
+
+      // console.log(newArrSix);
+    },
+  },
+});
+
+export const { setGridByFour, setGridBySix } = chooseGridSlice.actions;
