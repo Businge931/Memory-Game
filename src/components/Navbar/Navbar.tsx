d93c561c9a307e125ff44f0fe94843943f3eaadb
Businge931 <jimmybusinge9@gmail.com>
2024-10-18 08:47:53 +0300

    add button handlers

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,37 @@
 import React from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
+// import { setTheme } from "../../store/Settings/setThemeSlice";
+// import { setPlayerNumber } from "../../store/Settings/setPlayerNumberSlice";
+// import { setGridSize } from "../../store/Settings/setGridSlice";
+// import { useAppDispatch, useAppSelector } from "../../store/hooks";
 
-const Navbar = () => {
+const Navbar = ({ shuffleCards }: any) => {
+  // const dispatch = useAppDispatch();
+  // const theme = useAppSelector((state) => state.theme.themeName);
+  // const gridSize = useAppSelector((state) => state.chooseGrid.gridSize);
+  // const player = useAppSelector((state) => state.players.players);
+  // console.log(theme);
+  // console.log(gridSize);
+  // console.log(player);
+
+  const newGame = () => {
+    // dispatch(setTheme(null)); //check these
+    // dispatch(setPlayerNumber(0)); //check these
+    // dispatch(setGridSize([])); //check these
+  };
+
   return (
     <nav className="navbar">
       <h1>memory</h1>
       <div className="navbar-buttons">
-        <button className="restart-button">Restart</button>
+        <button className="restart-button" onClick={shuffleCards}>
+          Restart
+        </button>
         <Link to="/">
-          <button className="new-game-button">New Game</button>
+          <button className="new-game-button" onClick={newGame}>
+            New Game
+          </button>
         </Link>
       </div>
     </nav>
