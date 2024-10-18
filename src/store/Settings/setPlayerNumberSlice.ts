a1edde510aaaeeb6d6e5df9493b99580c9143f40
Businge931 <jimmybusinge9@gmail.com>
2024-10-18 08:52:04 +0300

    set player numbers

--- src/store/Settings/setPlayerNumberSlice.ts
+++ src/store/Settings/setPlayerNumberSlice.ts
@@ -1,1 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+interface players {
+  players: number;
+}
+
+const initialState: players = {
+  players: 0,
+};
+
+export const playersSlice = createSlice({
+  name: "players",
+  initialState,
+  reducers: {
+    setPlayerNumber: (state, action) => {
+      state.players = action.payload;
+    },
+  },
+});
+
+export const { setPlayerNumber } = playersSlice.actions;
