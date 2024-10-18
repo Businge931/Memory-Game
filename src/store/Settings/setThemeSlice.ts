0762f06a60969febb85a2eba1c738dca107deeec
Businge931 <jimmybusinge9@gmail.com>
2024-10-18 08:52:24 +0300

    create theme slice

--- src/store/Settings/setThemeSlice.ts
+++ src/store/Settings/setThemeSlice.ts
@@ -1,1 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
+
+interface themeTypes {
+  themeName: string | null;
+}
+const initialState: themeTypes = {
+  themeName: null,
+};
+
+export const themeSlice = createSlice({
+  name: "theme-slice",
+  initialState,
+  reducers: {
+    setTheme: (state, action) => {
+      state.themeName = action.payload;
+    },
+  },
+});
+
+export const { setTheme } = themeSlice.actions;
