8588888ab068d0ea2f085d71521c5dd3e957dade
Businge931 <jimmybusinge9@gmail.com>
2024-10-18 08:51:34 +0300

    add theme and player slices

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { activeButtonSlice } from "./activeButtonSlice";
 import { chooseGridSlice } from "./Settings/setGridSlice";
+import { playersSlice } from "./Settings/setPlayerNumberSlice";
+import { themeSlice } from "./Settings/setThemeSlice";
 
 export const store = configureStore({
   reducer: {
     activeButton: activeButtonSlice.reducer,
     chooseGrid: chooseGridSlice.reducer,
+    players: playersSlice.reducer,
+    theme: themeSlice.reducer,
   },
 });
 
