ea42d37758f4d3b96ff664b3398d396e6fb5dfc2
Businge931 <jimmybusinge9@gmail.com>
2024-10-18 08:52:41 +0300

    list icon types

---
+++ src/utils/icons.ts
@@ -0,0 +1,14 @@
+export interface IconTypes {
+  src: string;
+  matched: boolean;
+  id?: number;
+}
+
+export const Icons: IconTypes[] = [
+  { src: "../assets/house-solid.svg", matched: false },
+  { src: "../assets/circle-up-solid.svg", matched: false },
+  { src: "../assets/cart-shopping-solid.svg", matched: false },
+  { src: "../assets/car-solid.svg", matched: false },
+  { src: "../assets/shield-halved-solid.svg", matched: false },
+  { src: "../assets/calendar-days-solid.svg", matched: false },
+];
