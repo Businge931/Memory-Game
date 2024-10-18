03a44540af9bd9624fee0134e7696dc1e8ecfc3f
Businge931 <jimmybusinge9@gmail.com>
2024-10-18 08:51:48 +0300

    set grid numbers

--- src/store/Settings/setGridSlice.ts
+++ src/store/Settings/setGridSlice.ts
@@ -1,12 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { generateNumbers } from "../../utils/randomNumbers";
 
 interface gridState {
-  grid: number;
-  gridArray: number[];
+  gridSize: number;
+  gridArray: any[];
 }
 const initialState: gridState = {
-  grid: 16 | 36,
+  gridSize: 0,
   gridArray: [],
 };
 
@@ -14,21 +13,11 @@ export const chooseGridSlice = createSlice({
   name: "chooseGrid",
   initialState,
   reducers: {
-    setGridByFour: (state, action) => {
-      state.grid = 16;
-      const fourGrid = generateNumbers(4, 10);
-      state.gridArray.concat(fourGrid);
-
-      // console.log(newArrFour);
-    },
-    setGridBySix: (state) => {
-      state.grid = 36;
-      const sixGrid = generateNumbers(6, 20);
-      state.gridArray.concat(sixGrid);
-
-      // console.log(newArrSix);
+    setGridSize: (state, action) => {
+      state.gridSize = action.payload;
+      state.gridArray = state.gridArray.concat(action.payload.gridArray);
     },
   },
 });
 
-export const { setGridByFour, setGridBySix } = chooseGridSlice.actions;
+export const { setGridSize } = chooseGridSlice.actions;
